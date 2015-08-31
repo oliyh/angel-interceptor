5ea47268037620accb0dedf88fb5ac924b2fa5d7
Oliver Hine <git@oliy.co.uk>
2015-08-31 16:46:44 +0000

    Implementing angel/satisfy

--- src/angel/interceptor.clj
+++ src/angel/interceptor.clj
@@ -8,4 +8,20 @@
 (defn provides [interceptor & provisions]
   (assoc (pedestal-interceptor/interceptor interceptor) ::provides (set provisions)))
 
-(defn satisfy [service-map])
+
+(defn- reorder-interceptors [interceptors]
+  (let [provides (reduce (fn [m i] (if-let [p (::provides i)]
+                                     (into m (zipmap p (repeat i)))
+                                     m)) {} interceptors)]
+
+    (clojure.pprint/pprint provides)
+
+    (distinct (reduce (fn [c i]
+                        (if-let [r (::requires i)]
+                          (do (println "requirement for" r)
+                            (concat c (map provides r) [i]))
+                          (conj c i)))
+                      [] interceptors))))
+
+(defn satisfy [service-map]
+  (update service-map :io.pedestal.http/interceptors reorder-interceptors))
--- test/angel/interceptor_test.clj
+++ test/angel/interceptor_test.clj
@@ -4,27 +4,74 @@
             [io.pedestal.interceptor :as pedestal-interceptor]
             [io.pedestal.interceptor.helpers :refer [before]]))
 
-(defn- some-fn [_]
-  nil)
+(defn- something [_] nil)
+(def ^:private some-interceptor (before ::some-interceptor something))
+(def ^:private another-interceptor (before ::another-interceptor something))
+(def ^:private yet-another-interceptor (before ::yet-another-interceptor something))
 
 (deftest requires-test
   (testing "works on plain map"
-    (is (= (pedestal-interceptor/interceptor {:enter some-fn
+    (is (= (pedestal-interceptor/interceptor {:enter something
                                               ::angel/requires #{:authentication :authorisation}})
-           (angel/requires {:enter some-fn} :authentication :authorisation))))
+           (angel/requires {:enter something} :authentication :authorisation))))
 
   (testing "works on something that's already an interceptor"
-    (is (= (pedestal-interceptor/interceptor {:enter some-fn
+    (is (= (pedestal-interceptor/interceptor {:enter something
                                               ::angel/requires #{:authentication :authorisation}})
-           (angel/requires (before some-fn) :authentication :authorisation)))))
+           (angel/requires (before something) :authentication :authorisation)))))
 
 (deftest provides-test
   (testing "works on plain map"
-    (is (= (pedestal-interceptor/interceptor {:enter some-fn
+    (is (= (pedestal-interceptor/interceptor {:enter something
                                               ::angel/provides #{:authentication :authorisation}})
-           (angel/provides {:enter some-fn} :authentication :authorisation))))
+           (angel/provides {:enter something} :authentication :authorisation))))
 
   (testing "works on something that's already an interceptor"
-    (is (= (pedestal-interceptor/interceptor {:enter some-fn
+    (is (= (pedestal-interceptor/interceptor {:enter something
                                               ::angel/provides #{:authentication :authorisation}})
-           (angel/provides (before some-fn) :authentication :authorisation)))))
+           (angel/provides (before something) :authentication :authorisation)))))
+
+
+(deftest satisfy-test
+
+  (testing "leaves interceptors without requirements alone"
+    (let [service-map {:io.pedestal.http/interceptors [some-interceptor another-interceptor]}]
+      (is (= service-map
+             (angel/satisfy service-map)))))
+
+  (testing "reorders interceptors to put requires after the appropriate requires"
+    (let [first-interceptor (angel/provides some-interceptor :something)
+          second-interceptor (angel/requires another-interceptor :something)]
+
+      (is (= {:io.pedestal.http/interceptors [first-interceptor second-interceptor]}
+             (angel/satisfy {:io.pedestal.http/interceptors [second-interceptor first-interceptor]})))))
+
+  (testing "deals with multi-level dependencies"
+    (let [first-interceptor (angel/provides some-interceptor :something)
+          second-interceptor (angel/provides (angel/requires another-interceptor :something) :another-thing)
+          third-interceptor (angel/requires yet-another-interceptor :another-thing)]
+
+      (is (= {:io.pedestal.http/interceptors [first-interceptor second-interceptor third-interceptor]}
+             (angel/satisfy {:io.pedestal.http/interceptors [second-interceptor third-interceptor first-interceptor]})))))
+
+  (testing "deals with multiple dependencies"
+    (let [first-interceptor (angel/provides some-interceptor :something)
+          second-interceptor (angel/provides another-interceptor :another-thing)
+          third-interceptor (angel/requires yet-another-interceptor :something :another-thing)]
+
+      (is (= {:io.pedestal.http/interceptors [first-interceptor second-interceptor third-interceptor]}
+             (angel/satisfy {:io.pedestal.http/interceptors [second-interceptor third-interceptor first-interceptor]})))))
+
+  (testing "deals with repeated dependencies"
+    (let [first-interceptor (angel/provides some-interceptor :something)
+          second-interceptor (angel/requires another-interceptor :something)
+          third-interceptor (angel/requires yet-another-interceptor :something)]
+
+      (is (= {:io.pedestal.http/interceptors [first-interceptor second-interceptor third-interceptor]}
+             (angel/satisfy {:io.pedestal.http/interceptors [second-interceptor first-interceptor third-interceptor]})))
+
+      (is (= {:io.pedestal.http/interceptors [first-interceptor second-interceptor third-interceptor]}
+             (angel/satisfy {:io.pedestal.http/interceptors [second-interceptor third-interceptor first-interceptor]})))
+
+      (is (= {:io.pedestal.http/interceptors [first-interceptor third-interceptor second-interceptor]}
+             (angel/satisfy {:io.pedestal.http/interceptors [third-interceptor second-interceptor first-interceptor]}))))))
