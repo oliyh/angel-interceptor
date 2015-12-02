8a4c9be1fa5477dc854454731d53e9f142ce4f90
Oliver Hine <git@oliy.co.uk>
2015-12-02 11:32:14 +0000

    Simplified, added documentation

--- src/angel/interceptor.clj
+++ src/angel/interceptor.clj
@@ -3,48 +3,57 @@
   (:require ;;[io.pedestal.http :as bootstrap]
    [io.pedestal.interceptor :as pedestal-interceptor]))
 
-(defn requires [interceptor & requirements]
+(defn requires
+  "Specifies that the interceptor requires certain values to be provided
+   and will fail if they are not met."
+  [interceptor & requirements]
   (vary-meta interceptor
              assoc
              ::requires (set requirements)
              ::strict? true))
 
-(defn prefers [interceptor & requirements]
+(defn prefers
+  "Specifies that the interceptor prefers certain values to be provided
+   but will continue if they are not met."
+  [interceptor & requirements]
   (vary-meta interceptor
              assoc
              ::requires (set requirements)
              ::strict? false))
 
-(defn provides [interceptor & provisions]
+(defn provides
+  "Specifies that the interceptor provides certain values"
+  [interceptor & provisions]
   (vary-meta interceptor
              assoc
              ::provides (set provisions)))
 
+(defn- strict-assertion [requirement interceptor]
+  (when (-> interceptor meta ::strict?)
+    (throw (Exception. (format "No interceptor provides %s to satisfy %s"
+                               requirement
+                               (:name interceptor))))))
 
-(defn- reorder-interceptors [interceptors]
+(defn- reorder-interceptors
+  "Produces an interceptor chain where the interceptors which provide values
+   are placed before the interceptors which require them"
+  [interceptors]
   (let [providers (reduce (fn [m i] (if-let [p (-> i meta ::provides)]
                                       (into m (zipmap p (repeat i)))
                                       m)) {} interceptors)]
 
     (->> interceptors
          (reduce
-          (fn [c i]
-            (if-let [requirements (-> i meta ::requires)]
-              (into c
+          (fn [chain interceptor]
+            (if-let [requirements (-> interceptor meta ::requires)]
+              (into chain
                     (conj
                      (->> requirements
-                          (mapv (fn [r] (if-let [p (get providers r)]
-                                          p
-                                          (when (-> i meta ::strict?)
-                                            (throw (Exception.
-                                                    (format
-                                                     "No interceptor provides %s to satisfy %s"
-                                                     r
-                                                     (:name i))))))))
-                          (remove nil?)
+                          (keep #(or (get providers %)
+                                     (strict-assertion % interceptor)))
                           (into []))
-                     i))
-              (conj c i)))
+                     interceptor))
+              (conj chain interceptor)))
           [])
          distinct
          (into []))))
@@ -55,7 +64,10 @@
                 (throw (Exception. (str "Route" (pr-str %)) e))))
         routes))
 
-(defn satisfy [service-map]
+(defn satisfy
+  "Given a pedestal service-map returns an updated map where dependencies between
+   interceptors have been resolved by reordering"
+  [service-map]
   (cond-> service-map
 
     (:io.pedestal.http/interceptors service-map)
